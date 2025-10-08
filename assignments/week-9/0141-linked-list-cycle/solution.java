35143260eccbc906224c707d62969fe227c16ae6
Nitesh <niteshprimedev@gmail.com>
2025-10-08 16:44:03 +0530

    DSA Week-9 Assignment Questions

---
+++ assignments/week-9/0141-linked-list-cycle/solution.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slowNode = head;
+        ListNode fastNode = head;
+
+        while(fastNode != null && fastNode.next != null){
+            slowNode = slowNode.next;
+            fastNode = fastNode.next.next;
+
+            if(slowNode == fastNode){
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
